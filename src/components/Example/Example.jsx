2d46e916650e6f9caa2d92c20674fef12efe6e42
MatejGadjo <m.gadzovski@gmail.com>
2024-12-18 18:16:26 +0100

    Router setup

---
+++ src/components/Example/Example.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Example.css"
+
+const Example = () => {
+    return (
+        <div className="example">
+            <p>This is just an example!</p>
+        </div>
+    )
+}
+
+export default Example;
---
+++ src/pages/EventList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Example from "../components/Example/Example";
+
+const EventList = () => {
+    return(
+        <div>
+            <Example />
+        </div>
+    )
+}
+
+export default EventList;
